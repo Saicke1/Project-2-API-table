a9b7ab9af77163c778187f0dba135fe06eebc4de
Sarah Manicke <sm.wit2022@startsteps.de>
2022-09-13 07:36:30 +0200

    additional local API and finished checkboxes

--- JS/API.js
+++ JS/API.js
@@ -1,9 +1,11 @@
 const url = "https://mario-kart-tour-api.herokuapp.com/api/v1/drivers";
+const addInfo = additional;
 const tbody = document.getElementById("bodyTable");
 const searchBtn = document.getElementById("searchBtn");
 const clearBtn = document.getElementById("clearBtn");
 const input = document.getElementById("searchInput");
 const boxMustache = document.getElementById("checkMustache");
+const boxHat = document.getElementById("checkHat");
 let filterMustache
 let data
 
@@ -16,16 +18,16 @@ fetch(url).then(response => {
   return response.json()
 }).then(result => {  
   data = result
-  console.log(data)
+  //console.log(data)
 })
-setTimeout(displayTable, 2000);
+setTimeout(displayTable, 3000);
 }
 
 //DISPLAY THE WHOLE API DATA
 
 function displayTable(){
 data.forEach((driver, i) => {
-  console.log('driver', driver)
+  //console.log('driver', driver)
 
   //create Elements
   const tr = document.createElement("tr");
@@ -33,22 +35,34 @@ data.forEach((driver, i) => {
   const td2 = document.createElement("td");
   const td3 = document.createElement("td");
   const td4 = document.createElement("td");
+  const img = document.createElement("img");
+
+for(let g = 0; g < addInfo.length; g++){
+if(driver.name == addInfo[g].name){
+img.src = addInfo[g].img;
+break;
+} else {
+  img.src = "../Images/nintendo.svg.png";
+}
+}
 
 //define the inner HTML
-td1.innerHTML = i + 1;
 td2.innerHTML = driver.name;
 td3.innerHTML = driver.debut_tour;
 td4.innerHTML = driver.special_skill;
 
 //style the rows and columns
+tr.setAttribute("class", "rows");
 td1.setAttribute("class", "columStyle");
-    td1.setAttribute("id", "countStyle");
-    td2.setAttribute("class", "columStyle");
-    td3.setAttribute("class", "columStyle");
-    td4.setAttribute("class", "columStyle");
-    tr.setAttribute("class", "rows");
+td1.setAttribute("id", "countStyle");
+td2.setAttribute("class", "columStyle");
+td3.setAttribute("class", "columStyle");
+td4.setAttribute("class", "columStyle");
+img.setAttribute("class", "imageSize");
+    
 
   //append everything
+  td1.appendChild(img);
   tr.appendChild(td1);
   tr.appendChild(td2);
   tr.appendChild(td3);
@@ -58,6 +72,83 @@ td1.setAttribute("class", "columStyle");
 })
 const lakitu = document.getElementById("LakituDiv");
   lakitu.style.display = "none";
+}
+
+//FUNCTION CHECKBOX MUSTACHE
+
+boxMustache.addEventListener("click", showMustache)
+
+function showMustache(){
+  if(boxMustache.checked == true){
+    filterMustache = data.filter(driver => driver.mustache == true);
+    console.log(filterMustache)
+    tr = tbody.getElementsByTagName("tr");
+    for(let i = 0; i < filterMustache.length; i++){
+      for (let j = i; j < tr.length; j++) {
+        td = tr[j].getElementsByTagName("td")[1];
+        if(filterMustache[i].name == td.innerHTML && tr[j].className !== 'checkedMustache'){
+          tr[j].classList.add("checkedMustache");
+          console.log(tr[j]);
+        }}
+    }
+    for(let h = 0; h < tr.length; h++){
+      if(tr[h].classList.contains("checkedMustache") && tr[h].style.display == ""){
+        tr[h].style.display = "";
+      } else {
+        tr[h].style.display = "none";
+      }
+    }
+  } else {
+    tr = tbody.getElementsByTagName("tr");
+    for(let s = 0; s < tr.length; s++){
+      if(tr[s].classList.contains("checkedHat")){
+        tr[s].style.display = "";
+        tr[s].classList.remove = ("checkedMustache");
+      } else {
+        tr[s].style.display = "none";
+        tr[s].classList.remove = ("checkedMustache");
+      }
+    }
+  }
+}
+
+//FUNCTION CHECKBOX HAT
+
+boxHat.addEventListener("click", showHat)
+
+function showHat(){
+  if(boxHat.checked == true){
+    filterHat = data.filter(driver => driver.hat == true);
+    console.log(filterHat)
+    tr = tbody.getElementsByTagName("tr");
+    for(let i = 0; i < filterHat.length; i++){
+      for (let j = i; j < tr.length; j++) {
+        td = tr[j].getElementsByTagName("td")[1];
+        if(filterHat[i].name == td.innerHTML && tr[j].className !== 'checkedHat'){
+          tr[j].classList.add("checkedHat");
+          console.log(tr[j]);
+        }}
+    }
+    for(let h = 0; h < tr.length; h++){
+      if(tr[h].classList.contains("checkedHat") && tr[h].style.display == ""){
+        tr[h].style.display = "";
+        console.log(tr[h])
+      } else {
+        tr[h].style.display = "none";
+      }
+    }
+  } else {
+    tr = tbody.getElementsByTagName("tr");
+    for(let s = 0; s < tr.length; s++){
+      if(tr[s].classList.contains("checkedMustache")){
+        tr[s].style.display = "";
+        tr[s].classList.remove = ("checkedHat");
+      } else {
+        tr[s].style.display = "none";
+        tr[s].classList.remove = ("checkedHat");
+      }
+    }
+  }
 }
 
 //FUNCTION SEARCH BAR WITH SEARCH BUTTON
@@ -95,45 +186,4 @@ function clearing() {
     }
   }
   input.value = "";
-}
-
-//FUNCTION CHECKBOX MUSTACHE
-
-boxMustache.addEventListener("click", showMustache)
-
-function showMustache(){
-  if(boxMustache.checked == true){
-    filterMustache = data.filter(driver => driver.mustache == true);
-    //console.log(filterMustache)
-    tr = tbody.getElementsByTagName("tr");
-    for(let i = 0; i < filterMustache.length; i++){
-      //console.log(filterMustache[i].name)
-      for (let j = i; j < tr.length; j++) {
-        td = tr[j].getElementsByTagName("td")[1];
-        if(filterMustache[i].name == td.innerHTML){
-          tr[j].style.display = "";
-        } else {
-          tr[j].style.display = "none";
-        }}
-    }
-    /*filterMustache.forEach(must => {
-      console.log(must.name)
-    })
-    tr = tbody.getElementsByTagName("tr");
-    console.log(tr[1])
-    td = tr[1].getElementsByTagName("td")[1];
-    console.log(td)
-    console.log(td.innerHTML)*/
-   
-   
-   /* 
- filterMustache.forEach(must => {
-      
-      }
-    })
-  } else {
-    for (i = 0; i < tr.length; i++) {
-      tr[i].style.display = "none";
-      }*/
-  }
 }
--- JS/additional.js
+++ JS/additional.js
@@ -1,10 +1,607 @@
-var additional = {
-  1: {
-    img: "",
-    tex: "1",
+const additional = [
+  {
+    name: "Baby Mario",
+    img: "../Images/addImages/babyMario.png",
+    text: "1",
   },
-  2: {
-    img: "",
-    tex: "2",
+  {
+    name: "Baby Peach",
+    img: "../Images/addImages/babyPeach.webp",
+    text: "2",
   },
-};
+  {
+    name: "Baby Daisy",
+    img: "../Images/addImages/babyDaisy.webp",
+    text: "2",
+  },
+  {
+    name: "Baby Rosalina",
+    img: "../Images/addImages/babyRosalina.webp",
+    text: "2",
+  },
+  {
+    name: "Baby Luigi",
+    img: "../Images/addImages/babyLuigi.webp",
+    text: "2",
+  },
+  {
+    name: "Koopa Troopa",
+    img: "../Images/addImages/koopaTroopa.webp",
+    text: "1",
+  },
+  {
+    name: "Shy Guy",
+    img: "../Images/addImages/shyGuy.webp",
+    text: "2",
+  },
+  {
+    name: "Dry Bones",
+    img: "../Images/addImages/dryBones.webp",
+    text: "2",
+  },
+  {
+    name: "Iggy",
+    img: "../Images/addImages/iggy.webp",
+    text: "2",
+  },
+  {
+    name: "Larry",
+    img: "../Images/addImages/larry.webp",
+    text: "2",
+  },
+  {
+    name: "Lemmy",
+    img: "../Images/addImages/lemmy.webp",
+    text: "1",
+  },
+  {
+    name: "Ludwig",
+    img: "../Images/addImages/ludwig.webp",
+    text: "2",
+  },
+  {
+    name: "Morton",
+    img: "../Images/addImages/morton.webp",
+    text: "1",
+  },
+  {
+    name: "Roy",
+    img: "../Images/addImages/roy.webp",
+    text: "2",
+  },
+  {
+    name: "Wendy",
+    img: "../Images/addImages/wendy.webp",
+    text: "2",
+  },
+  {
+    name: "Mario",
+    img: "../Images/addImages/mario.webp",
+    text: "2",
+  },
+  {
+    name: "Peach",
+    img: "../Images/addImages/peach.webp",
+    text: "2",
+  },
+  {
+    name: "Yoshi",
+    img: "../Images/addImages/yoshi.webp",
+    text: "1",
+  },
+  {
+    name: "Daisy",
+    img: "../Images/addImages/daisy.webp",
+    text: "2",
+  },
+  {
+    name: "Toad",
+    img: "../Images/addImages/toad.webp",
+    text: "2",
+  },
+  {
+    name: "Toadette",
+    img: "../Images/addImages/toadette.webp",
+    text: "2",
+  },
+  {
+    name: "Rosalina",
+    img: "../Images/addImages/rosalina.webp",
+    text: "2",
+  },
+  {
+    name: "Luigi",
+    img: "../Images/addImages/luigi.webp",
+    text: "1",
+  },
+  {
+    name: "Toad (Pit Crew)",
+    img: "../Images/addImages/toadPitCrew.webp",
+    text: "2",
+  },
+  {
+    name: "Red Yoshi",
+    img: "../Images/addImages/redYoshi.webp",
+    text: "1",
+  },
+  {
+    name: "Blue Yoshi",
+    img: "../Images/addImages/blueYoshi.webp",
+    text: "2",
+  },
+  {
+    name: "Pink Yoshi",
+    img: "../Images/addImages/pinkYoshi.webp",
+    text: "2",
+  },
+  {
+    name: "Yellow Toad (Pit Crew)",
+    img: "../Images/addImages/yellowToadPitCrew.webp",
+    text: "2",
+  },
+  {
+    name: "Blue Toad (Pit Crew)",
+    img: "../Images/addImages/blueToadPitCrew.webp",
+    text: "2",
+  },
+  {
+    name: "Bowser",
+    img: "../Images/addImages/bowser.webp",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Mario",
+    img: "../Images/babyMario.png",
+    text: "1",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  },
+  {
+    name: "Baby Peach",
+    img: "",
+    text: "2",
+  }
+]
